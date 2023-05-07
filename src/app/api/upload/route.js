35d91eaa0e737a36f80041ad4f93eafbba422745
Ro1984Bi <bilbaorodrigojavi@outlook.com>
2023-05-07 13:17:19 -0400

    next project

---
+++ src/app/api/upload/route.js
@@ -0,0 +1,32 @@
+import { writeFile } from "fs/promises";
+import { NextResponse } from "next/server";
+import path from "path";
+
+export async function POST(request) {
+  try {
+    const data = await request.formData();
+    const file = data.get("file");
+
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    const filePath = path.join(process.cwd(), "public", file.name);
+    writeFile(filePath, buffer);
+    
+
+    return new Response(
+      JSON.stringify({
+        message: "Uploaded file",
+      })
+    );
+  } catch (error) {
+    return NextResponse.json(
+      JSON.stringify({
+        message: "no file",
+      }),
+      {
+        status: 400,
+      }
+    );
+  }
+}
